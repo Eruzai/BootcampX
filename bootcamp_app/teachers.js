3b5c9510559476e0182118c4e23911fbeb701316
Kevin Levasseur <kevin_levasseur@telus.net>
2024-03-13 10:53:26 -0700

    feat: sanitize inputs

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,9 +7,10 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2];
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`];
 
-pool.query(`
+const queryString = `
 SELECT
   DISTINCT teachers.name AS teacher,
   cohorts.name AS cohort
@@ -17,9 +18,11 @@ FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${cohortName || 'JUL02'}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
