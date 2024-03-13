2f24be30af336a9fd8d64920f1fa6c19ebdc1b63
Kevin Levasseur <kevin_levasseur@telus.net>
2024-03-13 10:53:12 -0700

    feat: sanitize inputs

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,18 +8,21 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
-const limit = process.argv[3];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
+const queryString = `
 SELECT 
   students.id AS id,
   students.name AS student,
   cohorts.name AS cohort
 FROM students
   JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${cohortName}%'
-LIMIT ${limit || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
